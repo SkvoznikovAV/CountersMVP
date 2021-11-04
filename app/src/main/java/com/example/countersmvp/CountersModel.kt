d93eae3656b2dba81a2cd6fc723b93bc5456875c
SkvoznikovAV <skvoznikovav@yandex.ru>
2021-11-04 13:22:07 +0400

    исправление архитектурной ошибки в презентере - передача индекса счетчика в обработчик щелчка

--- app/src/main/java/com/example/countersmvp/CountersModel.kt
+++ app/src/main/java/com/example/countersmvp/CountersModel.kt
@@ -1,18 +1,14 @@
 package com.example.countersmvp
 
 class CountersModel {
-    val counters = mutableListOf(0, 0, 0)
+    private val counters = mutableListOf(0, 0, 0)
 
-    fun getCurrent(index: Int): Int {
+    private fun getCurrent(index: Int): Int {
         return counters[index]
     }
 
     fun next(index: Int): Int {
         counters[index]++
         return getCurrent(index)
-    }
-
-    fun set(index: Int, value: Int){
-        counters[index] = value
     }
 }
